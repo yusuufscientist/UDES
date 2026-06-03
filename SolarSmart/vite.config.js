4009c0138e6259700a873734ede55c76764b104a
yusuufscientist <fcyusuuuf@gmail.com>
2026-06-03 05:50:20 +0100

    fix: build assets locally and force push build files

--- SolarSmart/vite.config.js
+++ SolarSmart/vite.config.js
@@ -4,11 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css', 
-                'resources/js/app.js'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    // هذا الجزء يمنع Vite من محاولة تحليل ملفات الـ Blade
+    build: {
+        rollupOptions: {
+            // نحدد فقط الملفات التي نريد بناءها
+            input: {
+                app: 'resources/js/app.js',
+                css: 'resources/css/app.css'
+            }
+        }
+    }
 });
